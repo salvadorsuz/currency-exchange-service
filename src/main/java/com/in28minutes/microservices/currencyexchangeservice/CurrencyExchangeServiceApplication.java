fcacb786de670bb8f0519522d9c249438b557cf6
salvadorsuz <salvadorsuz@yahoo.es>
2018-10-28 19:56:53 +0100

    Eureka config

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,11 +2,13 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.in28minutes.errorhandler.CustomizedResponseEntityExceptionHandler;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 	
 	public static void main(String[] args) {
